f45a7bf9e77d4aaef02bd605e75a7408c3e84210
Marcel <MTRNord@users.noreply.github.com>
2017-04-02 22:20:34 +0200

    Update pa.go

--- policy/pa.go
+++ policy/pa.go
@@ -325,8 +325,9 @@ func extractDomainIANASuffix(name string) (string, error) {
 	rule := publicsuffix.DefaultList.Find(name, &publicsuffix.FindOptions{IgnorePrivate: true, DefaultRule: nil})
 	
 	suffix := ""
-	
-	suffix2 := rule.Decompose(name)[1]
+	if rule != nil {
+		suffix2 := rule.Decompose(name)[1]
+	}
 	
 	if rule == nil {
 		sliced := strings.Split(name, ".")
